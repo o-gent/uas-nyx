11aa7e27198ff2d9c37000109615291b38794e3c
o-gent <olivergent@btinternet.com>
2020-12-19 23:30:39 +0000

    working multi-processing maybe

--- main.py
+++ main.py
@@ -13,16 +13,24 @@ idle -> payload drop -> idle -> speed test -> area search -> finish
 import numpy as np
 import cv2
 import target_recognition
+import multi
+import time
+
+
+k_nearest = target_recognition.load_model()
+target_recognition.k_nearest = k_nearest
 
 
 def take_image(__) -> np.ndarray:
     """ take an image from the camera and return it """
+    print("image taken")
     img = cv2.imread("test_images/field.png")
     return img
 
 
-def process_image(image, gps):
+def process_image(image):
     """ given an image return the colour, position, letter or none """
+    print("image recieved")
     result = target_recognition.findCharacters(image)
     if result:
         # work out position
@@ -31,9 +39,20 @@ def process_image(image, gps):
     return result
 
 
-def main():
-    pass
-
 
 if __name__ == "__main__":
-    main()
+
+    results = []
+    process = multi.WorkerManager(3, process_image)
+    take = multi.Worker(take_image)
+
+    take.send(1)
+    while True:
+        image = take.recieve()
+        if image is not None:
+            take.send(1)
+            process.add(image)
+        process.churn()
+        results.append(process.get())
+        time.sleep(1)
+
--- multi.py
+++ multi.py
@@ -5,6 +5,7 @@ functions and classes to manage multiple processes
 from multiprocessing import Process, Pipe
 import time
 import cv2
+from collections import deque
 
 
 def task(func, connection):
@@ -12,11 +13,13 @@ def task(func, connection):
     Base function for a task
     - function must accept connection arguement
     """
+    print(f"task started for {func}")
     while True:
         new = connection.recv()
-        if not new:
+        if type(new) == type(None):
             time.sleep(0.1)
             continue
+        
         result = func(new)
         connection.send(result)
 
@@ -26,25 +29,30 @@ class Worker:
     manages an individual process
     """
     def __init__(self, func):
-        self._send, self._recieve = Pipe()
+        print(f"Worker started for {func}")
+        self._home, self._away = Pipe()
         self.busy = False
-        self.process = Process(target=task, args=(func, self._recieve,))
+        self._process = Process(target=task, args=(func, self._away,))
+        self._process.start()
     
-    def send(self, item):
-        """ """
+    def send(self, item) -> bool:
+        """ if item is successfully sent to process then return true else false """
         if not self.busy:
-            self._send(item)
+            self._home.send(item)
             self.busy = True
             # indicate success
-            return
+            return True
         else:
             # indicate can't accept new
-            return
+            return False
     
     def recieve(self):
         """ check if worker has finished """
-        result = self._recieve.recv()
-        if result == None:
+        result = None
+        if self._home.poll(0.1):
+            result = self._home.recv()
+        
+        if type(result) == type(None):
             return None
         else:
             self.busy = False
@@ -56,5 +64,36 @@ class WorkerManager:
     Co-ordinates workers for multiprocess tasks 
     """
     
-    def __init__(self):
-        pass
+    def __init__(self, num_workers, function):
+        """ start num_workers amount of threads with passed function """
+        self._workers = deque([Worker(function) for i in range(num_workers)])
+        self._queue = []
+        self._results = []
+
+    def add(self, obj) -> int:
+        """ add to the queue, returns the length of the queue """
+        self._queue.append(obj)
+        return len(self._queue)
+
+    def get(self):
+        """ fetch the latest results, erasing current cache """
+        results = self._results
+        self._results = []
+        return results 
+
+    def churn(self):
+        """ process one queue item and fetch results """
+        try:
+            work = self._queue.pop(0) # get the oldest item
+        except:
+            return
+        
+        worker = self._workers[0]
+
+        result = worker.recieve()
+        if not worker.busy:
+            self._workers.rotate(1) # bring the oldest worker to the front
+            worker.send(work) # send new work to the free process
+            self._results.append(result)
+        else:
+            print("process was busy, skipping")
