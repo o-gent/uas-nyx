eda98005b5fbb8faa82d12fa28138ec5f3d75756
o-gent <olivergent@btinternet.com>
2021-07-02 09:07:42 +0100

    script corrections

--- scripts/camera_testing.py
+++ scripts/camera_testing.py
@@ -19,14 +19,15 @@ cam.start()
 
 try:
     while True:
-        cv2.imwrite(time.strftime(f"%m/%d %H:%M:%S {time.time_ns()}"),cam.read())
+        t = str(time.time()).split(".")[0]
+        cv2.imwrite(time.strftime(f"%m/%d %H:%M:%S {t}"),cam.read())
         logger.info("took image")
 
 except KeyboardInterrupt:
     logger.info("Keyboard interupt")
     cam.stop()
 
-except Exception:
-    logger.info("Failed")
+except Exception as e:
+    logger.info(f"Failed {e}")
     cam.stop()
 
